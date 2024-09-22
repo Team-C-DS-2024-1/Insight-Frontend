4165e7c87ccde762684e06b75a67f96d80640b96
Sebastian Andrade <sandradecedano@gmail.com>
2024-09-21 22:30:16 -0500

    Added api caller functions

--- src/api/BookApi.ts
+++ src/api/BookApi.ts
@@ -1,3 +1,5 @@
 import instance from "./axios";
 
-export const getAllBooks = async () => instance.get('/books');
+export const getAllBooks = async () => instance.get("/books");
+export const getBookByTitle = async (title: string) => instance.get(`/books/title/${title}`);
+export const getBookByIsbn = async (isbn: string) => instance.get(`/books/isbn/${isbn}`);
